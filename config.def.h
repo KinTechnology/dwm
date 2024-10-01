57c5aa2db261995df5078306cc823bc737c17ed2
Sadik Saifi <email@sadiksaifi.dev>
2024-10-02 01:00:32 +0530

    feat: add autostart patch

--- config.def.h
+++ config.def.h
@@ -18,6 +18,14 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"xrandr --output HDMI-1 --rotate inverted", NULL,
+	"xwallpaper --zoom /home/kindo/wall.png", NULL,
+	"kindo-app", NULL,
+	"unclutter -idle 1", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
